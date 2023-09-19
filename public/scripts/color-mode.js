204e84637bf892bffef14bc823cfd331718f53a1
alpay9 <alpay9393@gmail.com>
2023-09-19 17:36:51 +0300

    changed indentation

--- public/scripts/color-mode.js
+++ public/scripts/color-mode.js
@@ -1,42 +1,36 @@
 var icon = document.getElementById("iconsun");
 
 icon.onclick = async function () {
-  document.body.classList.toggle("dark-theme");
-  if (document.body.classList.contains("dark-theme")) {
-
-    console.log("selam");
-    try{
-        await fetch("/colormode", {
-          method: "POST",
-          body: JSON.stringify({
-            color: "dark",
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        icon.src = "/images/sun.png";
-
-    }catch(error){
-        console.log("ERROR HAPPENED");
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        try {
+            await fetch("/colormode", {
+                method: "POST",
+                body: JSON.stringify({
+                    color: "dark",
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            icon.src = "/images/sun.png";
+        } catch (error) {
+            console.log("ERROR: happened in dark color mode");
+        }
+    } else {
+        try {
+            await fetch("/colormode", {
+                method: "POST",
+                body: JSON.stringify({
+                    color: "daylight",
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            icon.src = "/images/moon.png";
+        } catch (error) {
+            console.log("ERROR: happened in daylight color mode");
+        }
     }
-    
-  } else {
-
-    try{
-        await fetch("/colormode", {
-          method: "POST",
-          body: JSON.stringify({
-            color: "daylight",
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        icon.src = "/images/moon.png";
-
-    }catch(error){
-        console.log("ERROR HAPPENED");
-    }
-  }
 };
