e6b853250374b2858662387291fec16b722bb50a
alpay9 <alpay9393@gmail.com>
2023-09-25 10:15:14 +0300

    prettified

--- controllers/base-controller.js
+++ controllers/base-controller.js
@@ -7,7 +7,12 @@ exports.getHome = (req, res) => {
 exports.getProblems = (req, res) => {
     Question.find()
         .then((questions) => {
-            res.render("general/list/questions", { questions: questions, headers: ["Title", "Description", "Difficulty"], attributes: ["title", "description", "difficulty"], editable: false });
+            res.render("general/list/questions", {
+                questions: questions,
+                headers: ["Title", "Description", "Difficulty"],
+                attributes: ["title", "description", "difficulty"],
+                editable: false,
+            });
         })
         .catch((err) => console.log(err));
 };
